fa7d1968301daa366bef635a354c829b5fa6cd2a
Devon Curtis <87959309+DevonCurtis@users.noreply.github.com>
2022-08-01 05:10:04 -0400

    added some bonus features

--- PreWork/ViewController.swift
+++ PreWork/ViewController.swift
@@ -8,15 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var result : String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     @IBOutlet weak var TextLabel: UILabel!
     @IBAction func ButtonClicked(_ sender: Any) {
         print("hello")
         TextLabel.textColor = UIColor.orange
+    }
+    @IBOutlet var ViewBackground: UIView!
+    @IBAction func ChangeView(_ sender: Any) {
+        ViewBackground.backgroundColor = UIColor.magenta
+    }
+    @IBOutlet weak var TextField: UITextField!
+    @IBOutlet weak var Label: UILabel!
+    @IBAction func ChangeText(_ sender: Any) {
+        result = TextField.text!
+        Label.text = "\(result)"
+        TextField.resignFirstResponder()
     }
 }
 
